3ca79538bdd7c5ba8f44413d4baf6cc0dd1ef117
Naveen Kumar Jallu <123621184+naveenkumarjallu2@users.noreply.github.com>
2026-09-06 17:34:21 +0530

    Delete supabase.js

--- supabase.js
+++
@@ -1,8 +0,0 @@
-// Supabase.js Configuration and Client
-const SUPABASE_URL = "https://gvvidndiuizrpyghuoyx.supabase.co";
-const SUPABASE_KEY = "sb_publishable_oOeXAhdowd-Oeah6787hxA_7dHioODf";
-
-const supabaseClient = supabase.createClient(
-    SUPABASE_URL,
-    SUPABASE_KEY
-);
